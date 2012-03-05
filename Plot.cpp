b0c49d04ccbc365251b2f4246a1c39dcf8fe2828
Jay Phillips <rocketman8473@gmail.com>
2012-03-05 01:03:29 -0500

    added coordinate system to Plot

--- Plot.cpp
+++ Plot.cpp
@@ -16,22 +16,26 @@ Plot* Plot::CreateInstance()
 Plot::Plot()
 {
 
-	gnuplot = popen("gnuplot -geometry 1080x1080 - > /dev/null 2>&1","w");
+	gnuplot = popen("gnuplot -geometry 600x600 - > /dev/null 2>&1","w");
 	//write("set term postscript eps color");
-	//write("set xlabel \"x\"");
+	//write("set xlabel \"x\" offset first 3,0,0");
 	write("set xrange[-3:3]");
-	//write("set ylabel \"y\"");
+	//write("set ylabel \"y\" offset first 0,3,0");
 	write("set yrange[-3:3]");
-	//write("set zlabel \"z\"");
+	//write("set zlabel \"z\" offset first 0,0,3");
 	write("set zrange[0:6]");
-	write("set view 90,0,1,1");
+	write("set view 90,0");
 	write("set view equal_axes xyz");
 	write("unset key");
 	write("set border 15");
 	write("set ticslevel 0");
-	write("unset xtics");
-	write("unset ytics");
-	write("unset ztics");
+	write("set zeroaxis lw 2 lt 1 lc rgb \"#000000\"");
+	write("set xtics 1 axis nomirror");
+	write("set ytics 1 axis nomirror");
+	write("set ztics 1 axis nomirror");
+//	write("unset xtics");
+//	write("unset ytics");
+//	write("unset ztics");
 	write("f(x,y) = 0");
 	write("splot f(x,y)");
 
